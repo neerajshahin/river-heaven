814cac5a52c9174fcda2cc27d77f14aa7a4c4c76
neeraj kumar <neeraj.kumar@informa.com>
2022-06-08 19:33:11 +0530

    updated desc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "River Heaven, Dehradun, Uttrakhand",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "River Heaven Colony Updates, Jhajra, Dehradun, Uttrakhand- 248007",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
